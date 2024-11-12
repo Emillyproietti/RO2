bea29674ff2f3a087da46dd259433add3db3a788
Emillyproietti <emillyandrade519@gmail.com>
2024-11-12 13:49:02 -0300

    Enviando o projeto para o repositório

--- src/main/java/br/senai/sp/gestaoro/controller/RoController.java
+++ src/main/java/br/senai/sp/gestaoro/controller/RoController.java
@@ -1,6 +1,7 @@
 package br.senai.sp.gestaoro.controller;
 
 import br.senai.sp.gestaoro.model.Ro;
+import br.senai.sp.gestaoro.repository.TurmaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RoController {
 
 
+    private final TurmaRepository turmaRepository;
+
+    public RoController(TurmaRepository turmaRepository) {
+        this.turmaRepository = turmaRepository;
+    }
+
     @GetMapping("/form-inserir")
     public String formInserir(Model model){
+        model.addAttribute("turmas", turmaRepository.findAll());
         model.addAttribute("ro", new Ro());
-
         return "ro/form-inserir";
     }
+
 
 }

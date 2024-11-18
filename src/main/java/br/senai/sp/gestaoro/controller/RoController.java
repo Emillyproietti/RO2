96c3c3ef130509f8105f05fc35f6047abcad6f11
Julyamiau <julyabeatrizmoreiraguerra@gmail.com>
2024-11-18 08:44:59 -0300

    testando o github

--- src/main/java/br/senai/sp/gestaoro/controller/RoController.java
+++ src/main/java/br/senai/sp/gestaoro/controller/RoController.java
@@ -1,6 +1,9 @@
 package br.senai.sp.gestaoro.controller;
 
 import br.senai.sp.gestaoro.model.Ro;
+import br.senai.sp.gestaoro.repository.RoRepository;
+import br.senai.sp.gestaoro.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/ro")
 public class RoController {
+    @Autowired
+    private RoRepository roRepository;
 
+    @GetMapping
+    public String listagem(Model model){
+        model.addAttribute("ro", roRepository.findAll());
+        return "ro/listagem";
+    }
 
     @GetMapping("/form-inserir")
     public String formInserir(Model model){

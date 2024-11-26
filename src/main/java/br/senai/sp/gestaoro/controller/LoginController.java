a2d364d4ba133f104a5e6dd466055b62ffee98de
Emillyproietti <emillyandrade519@gmail.com>
2024-11-26 13:32:28 -0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/br/senai/sp/gestaoro/controller/LoginController.java
+++ src/main/java/br/senai/sp/gestaoro/controller/LoginController.java
@@ -18,36 +18,5 @@ public class LoginController {
     }
 
 
-        @GetMapping("/solicitar-recuperacao-senha")
-        public String formRecuperarSenha(Model model) {
-            model.addAttribute("usuario", new User());
-            return "login/recuperar-senha";
-        }
-
-        @PostMapping("/solicitar-recuperacao-senha")
-        public String enviarEmailRecuperacao(
-                @RequestParam("email") String email) {
-            // Implementar lógica para enviar o email com o código de verificação
-            return "login/form-informar-email"; // Página de confirmação de envio do email
-        }
-
-        @GetMapping("/verificar-codigo")
-        public String verificarCodigo(@RequestParam("codigo") String codigo) {
-            // Validar o código
-            if (codigoValido(codigo)) {
-                return "login/recuperar-senha";
-            } else {
-                return "login/esquecisenha";
-            }
-        }
-
-        @PostMapping("/definir-nova-senha")
-        public String atualizarSenha(
-                @RequestParam("novaSenha") String novaSenha,
-                // Outros parâmetros necessários
-        ) {
-            // Atualizar a senha do usuário no banco de dados
-            return "login/form-informar-senha";
-        }
 
 }
